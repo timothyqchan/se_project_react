03c59359a7812fff0d3844f32b7e1103d4db9f5d
Timothy Chan <timothyqchan@gmail.com>
2024-03-12 18:58:10 -0400

    fix: auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,32 +5,20 @@ export const registration = ({ email, password, name, avatar }) => {
   return fetch(`${baseUrl}/signup`, {
     method: "POST",
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ name, avatar, email, password }),
-  })
-    .then(processServerResponse)
-    .catch((err) => {
-      console.log(err);
-      throw err;
-    });
+  }).then(processServerResponse);
 };
 
 export const authorize = ({ email, password }) => {
   return fetch(`${baseUrl}/signin`, {
     method: "POST",
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
-  })
-    .then(processServerResponse)
-    .catch((err) => {
-      console.log(err);
-      throw err;
-    });
+  }).then(processServerResponse);
 };
 
 export const profileUpdate = ({ name, avatar }) => {
